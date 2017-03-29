b2c75b72381858f10fff977f5eb3cfeb081725f4
Kenneth Walther Hansen <kenn3973@edu.easj.dk>
2017-03-29 13:49:36 +0200

    Oprettet Singleton og RelayCommand

---
+++ HotelBookingApp/Model/Guest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.Model
+{
+    class Guest
+    {
+        public int Guest_No { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public virtual ICollection<Booking> Bookings { get; set; }
+
+        public Guest(int Guest_no, string Name, string Address)
+        {
+            this.Guest_No = Guest_No;
+            this.Name = Name;
+            this.Address = Address;
+        }
+
+        public override string ToString()
+        {
+            return $"Guest: {Guest_No}, Name: {Name}, Address: {Address}";
+        }
+    }
+}
---
+++ HotelBookingApp/Model/HotelSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace HotelBookingApp.Model
+{
+    class HotelSingleton
+    {
+        private static HotelSingleton _instance;
+
+        public static HotelSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new HotelSingleton();
+                }
+                return _instance;
+            }
+        }
+
+        public ObservableCollection<Guest> Guests { get; set; }
+
+        private HotelSingleton()
+        {
+            Guests = new ObservableCollection<Guest>();
+        }
+    }
+}
